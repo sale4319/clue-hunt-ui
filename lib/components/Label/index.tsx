40c2a14bc43eb748187c1bb9a96512fa134b64aa
Aleksandar Stojanovic <sale4319@gmail.com>
2025-04-06 13:39:38 +0200

    feat: configure build and publish

---
+++ lib/components/Label/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export function Label(props: React.LabelHTMLAttributes<HTMLLabelElement>) {
+  const { className, ...restProps } = props;
+  return <label className={`${className} ${styles.label}`} {...restProps} />;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+
+// https://nodejs.org/api/packages.html#packages_self_referencing_a_package_using_its_name
+import { Button, Label, Input } from "clue-hunt-ui";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [inputCustomCountValue, setInputCustomCountValue] = useState("");
+
+  const handleClickCustomCount = () => {
+    if (inputCustomCountValue === "") {
+      setCount((count) => count + 1);
+    } else {
+      setCount(Number(inputCustomCountValue));
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Label>My Label</Label>
+      <br />
+      <Input
+        placeholder="Custom count"
+        value={inputCustomCountValue}
+        onChange={(e) => setInputCustomCountValue(e.target.value)}
+      />
+      <br />
+      <Button onClick={handleClickCustomCount}>count is {count}</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
